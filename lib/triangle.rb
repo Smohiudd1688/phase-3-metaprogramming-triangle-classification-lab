62fe2c1a50412198f7d9c0462f57325d23d8d40c
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-07-10 14:20:26 -0700

    commit tree f8a8214c3a237e6cb2d712b9631b40eaab374780

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
   # write code here
+  attr_reader :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind 
+    is_greater_than_zero = side1 > 0 && side2 > 0 && side3 > 0
+    is_triangle = side1 < side2 + side3 && side2 < side1 + side3 && side3 < side2 + side1 
+
+    if is_triangle && is_greater_than_zero
+      if side1 == side2 && side1 == side3
+        :equilateral
+      elsif side1 != side2 && side2 != side3 && side1 != side3
+        :scalene
+      else
+        :isosceles
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "All sides must be greater than 0, and follow the principle known as triangle inequality."
+    end
+  end
 end
